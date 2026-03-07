e39f1a03165a503cd44f4756f6d8bae29abd955f
V0calgiant12 <wyatturpone@gmail.com>
2026-03-06 18:20:07 -0800

    Exit Button
    Scripted Exit button functionality for both editor and application.
    Created and added MenuUIHandler to Title Screen game object.

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
