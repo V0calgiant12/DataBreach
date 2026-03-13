8aec432d48e289dd086475b8befeebaa89df4942
V0calgiant12 <wyatturpone@gmail.com>
2026-03-13 16:23:06 -0700

    Added sliders for Master, Music, Effects, and Dialogue. All functional.

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.VisualScripting;
+
 //using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -7,9 +9,32 @@ public class VolumeController : MonoBehaviour
 {
     [SerializeField] private AudioMixer Mixer;
     //[SerializeField] private TextMeshProUGUI ValueText;
+    [SerializeField] private VolumeType type;
+    public enum VolumeType
+    {
+        Master,
+        Music,
+        Effects,
+        Dialogue
+    }
     public void OnChangeSlider(float Value)
     {
         //ValueText.text = "Master Volume: " + $"{Value.ToString("N4")}";
-        Mixer.SetFloat("MasterVolume",MathF.Log10(Value)*20);
+        switch (type)
+        {
+            case VolumeType.Master:
+                Mixer.SetFloat("MasterVolume",MathF.Log10(Value)*20);
+                break;
+            case VolumeType.Music:
+                Mixer.SetFloat("MusicVolume",MathF.Log10(Value)*20);
+                break;
+            case VolumeType.Effects:
+                Mixer.SetFloat("EffectsVolume",MathF.Log10(Value)*20);
+                break;
+            case VolumeType.Dialogue:
+                Mixer.SetFloat("DialogueVolume",MathF.Log10(Value)*20);
+                break;
+        }
+        
     }
 }
