865586544fa9f3760123353041230c3d6b292159
V0calgiant12 <wyatturpone@gmail.com>
2026-03-13 16:10:59 -0700

    Commented uneccessary things

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,15 +1,15 @@
 using System;
-using TMPro;
+//using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class VolumeController : MonoBehaviour
 {
     [SerializeField] private AudioMixer Mixer;
-    [SerializeField] private TextMeshProUGUI ValueText;
+    //[SerializeField] private TextMeshProUGUI ValueText;
     public void OnChangeSlider(float Value)
     {
         //ValueText.text = "Master Volume: " + $"{Value.ToString("N4")}";
-        Mixer.SetFloat("Volume",MathF.Log10(Value)*20);
+        Mixer.SetFloat("MasterVolume",MathF.Log10(Value)*20);
     }
 }
