15ed584fb697144ddfb4192b0cd4fc6b06d26cbd
V0calgiant12 <wyatturpone@gmail.com>
2026-03-12 20:05:46 -0700

    Allowed Mouse binding

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -87,7 +87,7 @@ public class SettingsController : MonoBehaviour
     public void OnGUI()
     {
         Event e = Event.current;
-        if (e.isKey && e.keyCode != KeyCode.None && e.keyCode != KeyCode.Return && e.keyCode != KeyCode.Escape)
+        if ((e.isKey || e.isMouse) && e.keyCode != KeyCode.None && e.keyCode != KeyCode.Return && e.keyCode != KeyCode.Escape)
         {
             currentKeyDown = e.keyCode;
             Debug.Log(e.keyCode);
