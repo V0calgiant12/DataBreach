79daa8cf4280f4cf30c2095f1276f496be3680de
V0calgiant12 <wyatturpone@gmail.com>
2026-03-13 16:06:23 -0700

    Removed SliderRounder script and 2 lines in Volume Controller

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -6,11 +6,10 @@ using UnityEngine.Audio;
 public class VolumeController : MonoBehaviour
 {
     [SerializeField] private AudioMixer Mixer;
-    [SerializeField] private AudioSource AudioSource;
     [SerializeField] private TextMeshProUGUI ValueText;
     public void OnChangeSlider(float Value)
     {
-        ValueText.SetText($"{Value.ToString("N4")}");
+        //ValueText.text = "Master Volume: " + $"{Value.ToString("N4")}";
         Mixer.SetFloat("Volume",MathF.Log10(Value)*20);
     }
 }
