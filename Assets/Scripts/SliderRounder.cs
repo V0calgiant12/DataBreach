9511d68d7851b11a5f91a4c119734d698ff58b31
V0calgiant12 <wyatturpone@gmail.com>
2026-03-13 15:09:04 -0700

    Started working on Audio Tab

---
+++ Assets/Scripts/SliderRounder.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class SliderRounder : MonoBehaviour
+{
+    public void RoundValue(float value)
+    {
+        Debug.Log("Value changed to: " + value);
+    }
+}
