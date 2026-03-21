c3003750d41d9cbeb76bb1b4b8b80cb492eb7c19
V0calgiant12 <wyatturpone@gmail.com>
2026-03-20 19:57:27 -0700

    Post Processing options now functional.

---
+++ Assets/Scripts/Visuals/PostProcessingToggles.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessingToggles : MonoBehaviour
+{
+    [SerializeField] private Volume volume;
+    void Start()
+    {
+        if(volume.profile.TryGet(out Bloom bloom))
+        {
+            if (SettingsData.Instance._Bloom)
+            {
+                bloom.active = true;
+            }
+            else
+            {
+                bloom.active = false;
+            }
+        }
+        if(volume.profile.TryGet(out ChromaticAberration chromaticAberration))
+        {
+            if (SettingsData.Instance._Bloom)
+            {
+                chromaticAberration.active = true;
+            }
+            else
+            {
+                chromaticAberration.active = false;
+            }
+        }
+        if(volume.profile.TryGet(out Vignette vignette))
+        {
+            if (SettingsData.Instance._Bloom)
+            {
+                vignette.active = true;
+            }
+            else
+            {
+                vignette.active = false;
+            }
+        }
+    }
+}
