71a361c53a51e9d960bb223c17525b3df703a5fd
lucas <lucas.ramirez@cersd.org>
2026-03-25 15:18:48 -0700

    3/25/26 15:18 commit

--- Assets/Scripts/Enemies/Slime/SlimeStateManager.cs
+++ Assets/Scripts/Enemies/Slime/SlimeStateManager.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-public class Slime : MonoBehaviour
+public class SlimeStateManager : MonoBehaviour
 {
-
-    public Rigidbody2D enemyRb;
-    private GameObject player;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Player");
+        
     }
 
     // Update is called once per frame
