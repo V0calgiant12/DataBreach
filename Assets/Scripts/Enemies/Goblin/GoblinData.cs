eb28624617315d518191c4a86fc60d054c1640ef
lucas <lucas.ramirez@cersd.org>
2026-03-25 16:40:14 -0700

    goblin start commit
    gfrdn khihblvFWSvjbvdsbhkvdsnhvdbbfngf glibernocker dsunfjnauivajubnteabgn

--- Assets/Scripts/Enemies/Goblin/GoblinData.cs
+++ Assets/Scripts/Enemies/Goblin/GoblinData.cs
@@ -1,21 +1,16 @@
 using UnityEngine;
 
-public class Goblin : MonoBehaviour
+public class GoblinData : MonoBehaviour
 {
-
-    public Rigidbody2D enemyRb;
-    private GameObject player;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Player");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        
     }
 }
