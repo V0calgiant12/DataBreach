e91221814c36f181962ebadac536a8eee3ea94ed
V0calgiant12 <wyatturpone@gmail.com>
2026-03-25 16:26:34 -0700

    Removed kinda useless code

--- Assets/Scripts/Player/CameraUpdater.cs
+++ Assets/Scripts/Player/CameraUpdater.cs
@@ -12,14 +12,5 @@ public class CameraUpdater : MonoBehaviour
     void LateUpdate()
     {
         transform.position = Vector3.SmoothDamp(transform.position, cameraLoc.transform.position, ref Velocity, 0.2f);
-        // Stops camera from "Sliding"
-        if (!Input.anyKey && Velocity.x < 1 && Velocity.x > -1)
-        {
-            Velocity = new Vector2(0, Velocity.y);
-        }
-        if (!Input.anyKey && Velocity.y < 1 && Velocity.y > -1)
-        {
-            Velocity = new Vector2(Velocity.x, 0);
-        }
     }
 }
