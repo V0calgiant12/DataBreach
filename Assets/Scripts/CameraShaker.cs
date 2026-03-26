90abe4d5372ef434c4649b396405a35427e58303
V0calgiant12 <wyatturpone@gmail.com>
2026-03-26 09:06:00 -0700

    Attempting to change camera shake

--- Assets/Scripts/CameraShaker.cs
+++ Assets/Scripts/CameraShaker.cs
@@ -1,11 +1,20 @@
 using System.Collections;
+using System.Numerics;
 using UnityEngine;
 
 public class CameraShaker : MonoBehaviour
 {
+    public static CameraShaker Instance;
+    private UnityEngine.Vector3 Velocity = UnityEngine.Vector3.zero;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     public IEnumerator Shake(float duration, float magnitude)
     {
-        Vector3 originalLocalPosition = transform.localPosition;
+        UnityEngine.Vector3 originalLocalPosition = transform.localPosition;
         float elapsed = 0f;
 
         while (elapsed < duration)
@@ -13,11 +22,11 @@ public class CameraShaker : MonoBehaviour
             // Generate a random point inside a sphere and multiply by magnitude
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
-            float z = Random.Range(-1f, 1f) * magnitude;
 
-            transform.localPosition = originalLocalPosition + new Vector3(x, y, z);
+            transform.localPosition = UnityEngine.Vector3.SmoothDamp(transform.position, originalLocalPosition + new UnityEngine.Vector3(x, y, -10), ref Velocity, magnitude);
+            //transform.localPosition = originalLocalPosition + new Vector3(x, y, z);
 
-            elapsed += Time.deltaTime;
+            elapsed += 1; // We are not using Time.deltaTime in this project
 
             yield return null; // Wait until the next frame
         }
--- Assets/Scripts/TriggerShake.cs
+++ Assets/Scripts/TriggerShake.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 // Example of how to call the shake effect from another script
 public class TriggerShake : MonoBehaviour
 {
-    public CameraShaker cameraShaker;
-    public float shakeDuration = 0.5f;
-    public float shakeMagnitude = 0.4f;
+    public float shakeDuration = 30;
+    public float shakeMagnitude = 0f;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
             // Start the coroutine on the CameraShaker instance
-            StartCoroutine(cameraShaker.Shake(shakeDuration, shakeMagnitude));
+            StartCoroutine(CameraShaker.Instance.Shake(shakeDuration, shakeMagnitude));
         }
     }
 }
