2096b6527f4cdf32e1f199fc7b17eca6820adb1f
V0calgiant12 <wyatturpone@gmail.com>
2026-03-28 12:16:01 -0700

    Started on text box

---
+++ Assets/Scripts/Menus/TextBoxes/TextBoxAnimation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TextBoxAnimation : MonoBehaviour 
+{
+    public float _FinalSize;
+    public float _OpenSpeed;
+    void Start()
+    {
+        
+    }
+    public void Open()
+    {
+        
+    }
+    public void Close()
+    {
+        
+    }
+}
