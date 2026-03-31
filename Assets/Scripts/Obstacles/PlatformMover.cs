0460a1cfa79c6e3401acc90c7295730177ecd72b
V0calgiant12 <wyatturpone@gmail.com>
2026-03-31 14:06:20 -0700

    platform changes

--- Assets/Scripts/Obstacles/PlatformMover.cs
+++ Assets/Scripts/Obstacles/PlatformMover.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,7 +12,8 @@ public class PlatformMover : MonoBehaviour
     public Transform pointA;
     public Transform pointB;
     public float moveSpeed = 0.1f;
-    [SerializeField] private Vector3 nextPos;
+    private UnityEngine.Vector2 currentSpeed;
+    [SerializeField] private UnityEngine.Vector2 nextPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,20 +22,39 @@ public class PlatformMover : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
         Slime = GameObject.FindGameObjectWithTag("Slime");
         nextPos = pointB.position;
+        currentSpeed = new UnityEngine.Vector2(1,1);
+        StartCoroutine(MoveTowardsPoint());
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(Vector2.Distance(transform.position, nextPos));
-        transform.position = Vector2.MoveTowards(transform.position, nextPos, moveSpeed);
-        if (Vector2.Distance(transform.position, nextPos) < 0.01f)
+    }
+    private IEnumerator MoveTowardsPoint()
+    {
+        currentSpeed = new UnityEngine.Vector2((nextPos.x - transform.position.x)/moveSpeed,(nextPos.y - transform.position.y)/moveSpeed);
+        while(UnityEngine.Vector2.Distance(transform.position,nextPos) > 1)
         {
-            nextPos = (nextPos == pointA.position) ? pointB.position : pointA.position;
+            Debug.Log(UnityEngine.Vector2.Distance(transform.position,nextPos));
+            platformRb.linearVelocityX = currentSpeed.x;
+            platformRb.linearVelocityY = currentSpeed.y;
+            yield return null;
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.CompareTag("PointA"))
+        {
+            nextPos = pointB.position;
+            StartCoroutine(MoveTowardsPoint());
+        }
+        if(other.gameObject.CompareTag("PointB"))
+        {
+            nextPos = pointA.position;
+            StartCoroutine(MoveTowardsPoint());
+        }
+
         if(other.gameObject.CompareTag("GroundCheck"))
         {
             Player.transform.parent = transform;
@@ -44,7 +66,13 @@ public class PlatformMover : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        Player.transform.parent = null;
-        Slime.transform.parent = null;
+        if(other.gameObject.CompareTag("GroundCheck"))
+        {
+            Player.transform.parent = null;
+        }
+        if(other.gameObject.CompareTag("SlimeTrigger"))
+        {
+            Slime.transform.parent = null;
+        }
     }
 }
