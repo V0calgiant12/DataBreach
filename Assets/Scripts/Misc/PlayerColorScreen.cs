3afc9d3d96683186a9cd530606d34a931704d221
V0calgiant12 <wyatturpone@gmail.com>
2026-03-30 22:44:07 -0700

    Finished intro cutscene.

---
+++ Assets/Scripts/Misc/PlayerColorScreen.cs
@@ -0,0 +1,19 @@
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerColorScreen : MonoBehaviour 
+{
+    [SerializeField] private UnityEngine.Color playerColor;
+    [SerializeField] private Image image;
+    [SerializeField] private Image parentImage;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        playerColor = UnityEngine.Color.HSVToRGB(SettingsData.Instance._PlayerHue, SettingsData.Instance._PlayerSaturation, SettingsData.Instance._PlayerValue);
+        image.color = new UnityEngine.Color(playerColor.r,playerColor.g,playerColor.b, parentImage.color.a);
+    }
+}
