be616aad182575fb5dc2cc638959bd955ee1fc0c
V0calgiant12 <wyatturpone@gmail.com>
2026-04-16 14:06:24 -0700

    Touch ups.

--- Assets/Scripts/Misc/SceneTransitionArea.cs
+++ Assets/Scripts/Misc/SceneTransitionArea.cs
@@ -4,13 +4,14 @@ using static System.TimeZoneInfo;
 public class SceneTransitionArea : MonoBehaviour
 {
     [SerializeField] private SceneTransition sceneTransition; 
-   
+    [SerializeField] private PlayerData playerData;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Entered Transition Area");
             sceneTransition.TransitionToScene(0, 1);
+            playerData.movementAllowed = false;
         }
     }
 
