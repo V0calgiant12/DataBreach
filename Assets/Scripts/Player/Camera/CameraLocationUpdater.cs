adbbcd994cb77409eef8e0d2670d237fa94b3080
V0calgiant12 <wyatturpone@gmail.com>
2026-04-17 18:14:33 -0700

    Camera limiter trigger
    Needs polishing.

--- Assets/Scripts/Player/Camera/CameraLocationUpdater.cs
+++ Assets/Scripts/Player/Camera/CameraLocationUpdater.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Numerics;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraLocationUpdater : MonoBehaviour 
@@ -6,12 +8,21 @@ public class CameraLocationUpdater : MonoBehaviour
     [Header("Camera Location Updater References:")]
     [SerializeField] private GameObject player;
     [SerializeField] private Rigidbody2D rb;
+    private Camera mainCamera;
+    public float limitX = float.NaN;
+    private float cameraWidth;
     void Start()
     {
-        
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
     void Update()
     {
+        cameraWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        Debug.Log(transform.position.x + cameraWidth);
         transform.position = new UnityEngine.Vector3(player.transform.position.x + rb.linearVelocityX/4, player.transform.position.y + rb.linearVelocityY/4, -10);
+        if(limitX != float.NaN && MathF.Abs(limitX) < transform.position.x + cameraWidth)
+        {
+            transform.position = new UnityEngine.Vector3(limitX - cameraWidth, transform.position.y, -10);
+        }
     }
 }
--- Assets/Scripts/TriggersAreas/CameraAreaTrigger.cs
+++ Assets/Scripts/TriggersAreas/CameraAreaTrigger.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class CameraAreaTrigger : MonoBehaviour
 {
+    [SerializeField] private float limitX;
+    [SerializeField] private float limitY;
+    [SerializeField] private float flippable;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Camera"))
+        if (other.gameObject.CompareTag("MainCamera"))
         {
             Debug.Log("Entered Transition Area");
+            CameraLocationUpdater cameraLocation = other.gameObject.GetComponent<CameraLocationUpdater>();
+            cameraLocation.limitX = limitX;
         }
     }
 }
