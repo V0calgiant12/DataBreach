2b869df0a0f372a4c1abc65554923c68fa0ad766
Gandalt <chrisnorth2169@gmail.com>
2026-04-22 14:06:43 -0700

    Start of descriptions for parts of code
    yayayy

--- Assets/Scripts/Obstacles/Sawblade/SawbladeHitbox.cs
+++ Assets/Scripts/Obstacles/Sawblade/SawbladeHitbox.cs
@@ -5,10 +5,12 @@ public class SawbladeHitbox : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // If the player is hit by the hitbox, it will call a function that subtracts 1 health and launches the player
         if(other.gameObject.CompareTag("Player")) 
         {
             PlayerStateManager.Instance.DamagePlayer(10, Random.Range(6,10),60,false,transform.position.x,false);
         }
+        // If an enemy is hit by the hitbox, it will call a function that subtracts 1 health and launches the enemy
         if(other.gameObject.CompareTag("EnemyHurtbox"))
         {
             Debug.Log(other.gameObject);
