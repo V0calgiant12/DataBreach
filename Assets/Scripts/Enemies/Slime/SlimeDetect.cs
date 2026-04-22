785fcdff64784b875c4c5c27b684c27649171188
V0calgiant12 <wyatturpone@gmail.com>
2026-04-22 08:07:56 -0700

    A lot.
    Ill try to get them all
    Fixed stalagmites sounds
    Stalagmites now hit enemies
    New Enemy Stalagmite that detects enemies. Doesn't hit player.
    New Enemy Hit script. Handles health of enemy.
    Reintroduced sorting layers.
    Hearts now have a glow.
    More Mountains level progress.
    Spikes now knock the player up the same amount every time (~3 tiles high)
    Removed some debug logs.

--- Assets/Scripts/Enemies/Slime/SlimeDetect.cs
+++ Assets/Scripts/Enemies/Slime/SlimeDetect.cs
@@ -6,11 +6,8 @@ public class SlimeDetect : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("TEST");
-        Debug.Log(other.gameObject);
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("TriggerEnter");
             slime.currentState = SlimeStateManager.State.Chase;
         }
     }
@@ -18,7 +15,6 @@ public class SlimeDetect : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("TriggerExit");
             slime.currentState = SlimeStateManager.State.Idle;
         }
     }
