d68b1700fa6982fa551438f8664288f0f1a301e2
V0calgiant12 <wyatturpone@gmail.com>
2026-04-30 13:15:12 -0700

    Scene Transition Trigger now has customize-ablity

--- Assets/Scripts/TriggersAreas/SceneTransitionArea.cs
+++ Assets/Scripts/TriggersAreas/SceneTransitionArea.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-using static System.TimeZoneInfo;
 
 public class SceneTransitionArea : MonoBehaviour
 {
     [SerializeField] private SceneTransition sceneTransition; 
     [SerializeField] private PlayerData playerData;
+    [SerializeField] private int sceneId;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Entered Transition Area");
-            sceneTransition.TransitionToScene(0, 1);
+            sceneTransition.TransitionToScene(sceneId, 1);
             playerData.movementAllowed = false;
         }
     }
