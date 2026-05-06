a5ef8e2e37c148cf8e539f6809507f7c7967ec26
Gandalt <chrisnorth2169@gmail.com>
2026-05-06 13:56:19 -0700

    Fixed Keagan's thing

--- Assets/Scripts/Misc/SceneTransition.cs
+++ Assets/Scripts/Misc/SceneTransition.cs
@@ -26,21 +26,22 @@ public class SceneTransition : MonoBehaviour
         {
             case(3):
                 // current level to 1 (Plains)
-                
+                break;
             case(4):
                 // current level to 2 (Forest)
-       
+                break;
             case(5):
                 //  current level to 3 (Mountains)
-       
+                break;
             case(6):
                 // current level to 4 (Quarry)
-        
+                break;
             case(7):
                 // current level to 5 (Deep Forest)
-       
+                break;
             case(8):
                 // current level to 6 (Corrupted Plains)
+                break;
         }
     }
     public void ExitButton()
