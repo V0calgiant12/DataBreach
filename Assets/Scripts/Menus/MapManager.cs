0c96b11fe0bc7f4a7b782b048ec679438a896253
V0calgiant12 <wyatturpone@gmail.com>
2026-05-17 19:38:37 -0700

    Map now has labels

--- Assets/Scripts/Menus/MapManager.cs
+++ Assets/Scripts/Menus/MapManager.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     [SerializeField] private SceneTransition sceneTransition;
+    private AudioSource audioSource;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void Forest()
     {
+        audioSource.Play();
         sceneTransition.TransitionToScene(4,1);
     }
     public void Mountains()
     {
+        audioSource.Play();
         sceneTransition.TransitionToScene(5,1);
     }
 }
