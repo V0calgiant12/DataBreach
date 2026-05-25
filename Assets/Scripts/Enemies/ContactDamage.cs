fbb1bf53e8e2e3f8c3fd34d45609259fa0ec09a9
V0calgiant12 <wyatturpone@gmail.com>
2026-05-25 14:25:45 -0700

    Text Trigger Areas can now heal the player.

--- Assets/Scripts/Enemies/ContactDamage.cs
+++ Assets/Scripts/Enemies/ContactDamage.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class ContactDamage : MonoBehaviour
 {
-    public AudioSource audioSource3;
     [SerializeField] private bool StopAtOneHp = false;
 
     void OnTriggerStay2D(Collider2D collision)
@@ -12,7 +11,6 @@ public class ContactDamage : MonoBehaviour
         {
             if(!StopAtOneHp || StopAtOneHp && PlayerStateManager.Instance.playerData.playerHealth != 1)
             {
-                audioSource3.Play();
                 PlayerStateManager.Instance.DamagePlayer(Random.Range(10,15),Random.Range(5,10), 90, false, transform.position.x, false);
             }
         }
--- Assets/Scripts/TriggersAreas/TextTriggerArea.cs
+++ Assets/Scripts/TriggersAreas/TextTriggerArea.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class TextTriggerArea : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private bool Triggered = false;
     [SerializeField] private bool CanRepeat = false;
     [SerializeField] private bool DamagePlayer = false;
+    [SerializeField] private bool HealPlayer = false;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && !Triggered)
@@ -21,6 +23,11 @@ public class TextTriggerArea : MonoBehaviour
             if(DamagePlayer)
             {
                 PlayerStateManager.Instance.DamagePlayer(0,0,60,true,transform.position.x,true);
+            }
+            if (HealPlayer)
+            {
+                audioSource.Play();
+                PlayerStateManager.Instance.playerData.playerHealth += 1;
             }
         }
     }
