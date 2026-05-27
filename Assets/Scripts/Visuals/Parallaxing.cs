917fb1560e7affce98cadd7cfc951bf2ffafc592
V0calgiant12 <wyatturpone@gmail.com>
2026-05-27 13:48:46 -0700

    Fixed suttering background

--- Assets/Scripts/Visuals/Parallaxing.cs
+++ Assets/Scripts/Visuals/Parallaxing.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 [ExecuteAlways]
 public class Parallaxing : MonoBehaviour
 {
     [SerializeField] private GameObject cameraPos;
     public Vector2 _Offset = new Vector2(0,0);
+    private Vector3 Velocity = Vector3.zero;
     public float parallaxX = 5;
     public float parallaxY = 5;
     void Update()
     {
-        transform.position = new Vector2((cameraPos.transform.position.x + _Offset.x) / parallaxX, (cameraPos.transform.position.y + _Offset.y) / parallaxY);
+        Vector3 newPos = new Vector3((cameraPos.transform.position.x + _Offset.x) / parallaxX, (cameraPos.transform.position.y + _Offset.y) / parallaxY,0);
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref Velocity, 0.05f);
     }
 }
