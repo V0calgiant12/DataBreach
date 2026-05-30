a44c931fa18f14b241aaee1f02f49695ac0f69d5
V0calgiant12 <wyatturpone@gmail.com>
2026-05-30 11:32:23 -0700

    Health now no longer animates on load.

--- Assets/Scripts/Menus/HealthHud.cs
+++ Assets/Scripts/Menus/HealthHud.cs
@@ -1,9 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class HealthHud : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
+    void Start()
+    {
+        animator.SetBool("SceneStarted", false);
+        StartCoroutine(LateStart());
+    }
+    IEnumerator LateStart()
+    {
+        int elapsed = 0;
+        while(elapsed <= 2)
+        {
+            elapsed += 1;
+            yield return null;
+        }
+        animator.SetBool("SceneStarted", true);
+    }
     void OnGUI()
     {
         animator.SetInteger("PlayerHealth", PlayerStateManager.Instance.playerData.playerHealth);
