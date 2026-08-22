18ea62928173f86391733f71285c01a4a77178b5
V0calgiant12 <wyatturpone@gmail.com>
2026-08-22 04:29:57 -0700

    Updated Para-Slimes.
    Now use MovingPlatform script to move.
    Made necessary changes to account for that.
    Removed unnecessary variables from MovingPlatform.cs.

--- Assets/Scripts/Enemies/Slime/ParaSlime.cs
+++ Assets/Scripts/Enemies/Slime/ParaSlime.cs
@@ -3,35 +3,15 @@ using UnityEngine;
 
 public class ParaSlime : MonoBehaviour
 {
-    [Header("Para Slime Settings:")]
-    public float platformSpeed = 1;
-    public float moveSpeed = 0.1f;
-    [Header("Para Slime References:")]
-    public Transform pointA;
-    public Transform pointB;
-    
     [SerializeField] private Vector3 nextPos;
     [SerializeField] private AudioSource audioSource;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        nextPos = pointB.position;
         InvokeRepeating(nameof(PlaySound),0.5f,0.5f);
     }
     private void PlaySound()
     {
         audioSource.pitch = Random.Range(0.7f,1.3f);
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //Debug.Log(Vector2.Distance(transform.position, nextPos));
-        transform.position = Vector2.MoveTowards(transform.position, nextPos, moveSpeed);
-        if (Vector2.Distance(transform.position, nextPos) < 0.01f)
-        {
-            nextPos = (nextPos == pointA.position) ? pointB.position : pointA.position;
-        }
     }
 }
