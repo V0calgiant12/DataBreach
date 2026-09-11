b10e9139f213b32eae2c6cfeb25a3cc50c45f71e
V0calgiant12 <wyatturpone@gmail.com>
2026-09-11 14:32:36 -0700

    Bit AI Change

--- Assets/Scripts/NPCs/BitAI.cs
+++ Assets/Scripts/NPCs/BitAI.cs
@@ -6,17 +6,29 @@ public class BitAI : MonoBehaviour
 {
     [Header("Bit Settings:")]
     public float moveSpeed;
+    public bool waitForPlayer = false;
     [Header("Bit References:")]
-    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject playerOverride;
     [SerializeField] private Rigidbody2D rb;
     
     void Start()
     {
-        StartCoroutine(MoveTowardsPlayer());
+        if(!waitForPlayer)
+        {
+            StartCoroutine(MoveTowardsPlayer());
+        }
     }
     void Update()
     {
-        transform.Rotate(0,0,rb.linearVelocityX+rb.linearVelocityY);
+        transform.Rotate(0,0,(rb.linearVelocityX+rb.linearVelocityY)*Time.timeScale);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(waitForPlayer && other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(MoveTowardsPlayer());
+            waitForPlayer = false;
+        }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
@@ -27,6 +39,7 @@ public class BitAI : MonoBehaviour
     }
     private IEnumerator MoveTowardsPlayer()
     {
+        GameObject player = playerOverride != null ? PlayerStateManager.Instance.gameObject : playerOverride;
         while(Vector2.Distance(transform.position,player.transform.position) > 2.5)
         {
             rb.linearVelocityX = moveSpeed*moveSpeed*moveSpeed * (player.transform.position.x - transform.position.x);
